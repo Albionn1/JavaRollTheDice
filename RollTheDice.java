e4cf01600643328a542300586408f92b7b479a3e
Albionn1 <57451316+Albionn1@users.noreply.github.com>
2021-01-02 02:15:57 +0100

    Add files via upload

--- RollTheDice.java
+++ RollTheDice.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 import java.awt.event.*;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 public class RollTheDice {
 	public static void gjuajeZarin(){
 
@@ -9,21 +11,22 @@ public class RollTheDice {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		short lojtari1 = sc.nextShort();
+		int lojtari1 = sc.nextShort();
 		rezultatiIZarit1();
 		
 		System.out.println("Lojtari i dyte te luaje duke shenuar numrin 2");
 		
-		short lojtari2 = sc.nextShort();
+		int lojtari2 = sc.nextShort();
 		
 		rezultatiIZarit2();
 	}
 	
 	public static void rezultatiIZarit1() {
 		
-		short hedhja1 = (short)(6* Math.random() + 1);
+		int hedhja1 = (int)(6* Math.random() + 1);
 		//Shtimi i numrave te zarave te arrayLista rezultati1 ma posht
 		rezultati1.add(hedhja1);
+
 		//hedhja1 =
 		if(hedhja1 == 6) {
 			
@@ -33,7 +36,7 @@ public class RollTheDice {
 			System.out.println(katrori_i_shtepise1);
 			
 					
-		}else if(hedhja1 == 5) {
+		}else if(hedhja1 == 5 & rezultati1.contains(6)) {
 			
 				
 				String kulmi_i_shtepise1 = (
@@ -42,13 +45,13 @@ public class RollTheDice {
 				System.out.println(kulmi_i_shtepise1);
 		
 				 
-		}else if(hedhja1 == 4) {
+		}else if(hedhja1 == 4 & rezultati1.contains(5) & rezultati1.contains(6)){
 
 
 			String dera_e_shtepise1 = "\t  \b\b\b\b\b\b\b\b \b/\\\n\t\b\b\b\b\b\b\b/\t\b\b\b\b\\\n -----\n|  _  |\n| | | |\n -----";
 
 			System.out.println(dera_e_shtepise1);
-		}else if(hedhja1 == 3) {
+		}else if(hedhja1 == 3 & rezultati1.contains(4) & rezultati1.contains(5) & rezultati1.contains(6)) {
 
 			String dritarja = ("\t  \b\b\b\b\b\b\b\b \b/\\\n\t\b\b\b\b\b\b\b/\t\b\b\b\b\\\n -----\n|  _  |\n|x| |x|\n -----");
 
@@ -59,38 +62,38 @@ public class RollTheDice {
 			System.out.println("nuk ke hedhur ndonje numer fitoreje");
 		}
 		System.out.println("Hedhja e pare: " + hedhja1);
+		Collections.sort(rezultati1);
 		System.out.println(rezultati1);
 	}
 	
 	
 	public static void rezultatiIZarit2() {
-		short hedhja2 = (short)(6* Math.random() + 1);
+		int hedhja2 = (int)(6* Math.random() + 1);
 		rezultati2.add(hedhja2);
 		//hedhja1 =
 		if(hedhja2 == 6) {
 			
-					String katrori_i_shtepise1 = (
+					String katrori_i_shtepise2 = (
 							" -----\n|     |\n|     |\n -----"
 							);
-			System.out.println(katrori_i_shtepise1);
+			System.out.println(katrori_i_shtepise2);
 			
 					
-		}else if(hedhja2 == 5) {
-			
+		}else if(hedhja2 == 5 & rezultati1.contains(6)) {
 				
-				String kulmi_i_shtepise1 = (
+				String kulmi_i_shtepise2 = (
 					"\t  \b\b\b\b\b\b\b\b \b/\\\n\t\b\b\b\b\b\b\b/\t\b\b\b\b\\\n -----\n|     |\n|     |\n -----"
 						);
-				System.out.println(kulmi_i_shtepise1);
+				System.out.println(kulmi_i_shtepise2);
 		
 				 
-		}else if(hedhja2 == 4) {
+		}else if(hedhja2 == 4 & rezultati1.contains(6) & rezultati1.contains(5)){
 
 
-			String dera_e_shtepise1 = "\t  \b\b\b\b\b\b\b\b \b/\\\n\t\b\b\b\b\b\b\b/\t\b\b\b\b\\\n -----\n|  _  |\n| | | |\n -----";
+			String dera_e_shtepise2 = "\t  \b\b\b\b\b\b\b\b \b/\\\n\t\b\b\b\b\b\b\b/\t\b\b\b\b\\\n -----\n|  _  |\n| | | |\n -----";
 
-			System.out.println(dera_e_shtepise1);
-		}else if(hedhja2 == 3) {
+			System.out.println(dera_e_shtepise2);
+		}else if(hedhja2 == 3 & rezultati1.contains(4) & rezultati1.contains(5) & rezultati1.contains(6)){
 
 			String dritarja = ("\t  \b\b\b\b\b\b\b\b \b/\\\n\t\b\b\b\b\b\b\b/\t\b\b\b\b\\\n -----\n|  _  |\n|x| |x|\n -----");
 
@@ -101,10 +104,11 @@ public class RollTheDice {
 			System.out.println("nuk ke hedhur ndonje numer fitoreje");
 		}
 		System.out.println("Hedhja e dyte: " + hedhja2);
+		Collections.sort(rezultati2);
 		System.out.println(rezultati2);
 	}
-	private static List<Short> rezultati2 = new ArrayList<>();
-	private static List<Short> rezultati1 = new ArrayList<>();
+	private static List<Integer> rezultati2 = new ArrayList<>();
+	private static List<Integer> rezultati1 = new ArrayList<>();
 	public static void main(String [] args) {
 		//System.out.println(" -----\n|     |\n|     |\n -----");
 		//while()
